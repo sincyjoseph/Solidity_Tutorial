555c99c8899aae24f1f6e773cb303cea01991d50
SINCY JOSEPH <sincyjoseph535@gmail.com>
2020-12-27 18:43:11 +0530

    Create EmployeeData2.sol
    Use of modifier function in employee data

---
+++ EmployeeData2.sol
@@ -0,0 +1,40 @@
+pragma solidity >= 0.5.0;
+
+contract EmployeeData{
+
+    address owner;
+    struct EmployeeDB{
+        uint employeeID;
+        string employeeName;
+        string employeeDesignation;
+    }
+    
+    function Demo() public{
+        owner = msg.sender;
+    }
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    }
+    
+    
+    event notification(
+        uint employeeID,
+        string employeeName,
+        string employeeDesignation
+        );
+    mapping(uint => EmployeeDB) public employeeMapping;
+    
+    function setEmployeeDetails(uint _employeeID, string memory _employeeName, string memory _employeeDesignation) public onlyOwner{
+        EmployeeDB storage employee = employeeMapping[_employeeID];
+        employee.employeeID = _employeeID;
+        employee.employeeName = _employeeName;
+        employee.employeeDesignation = _employeeDesignation;
+        emit notification(_employeeID, _employeeName, _employeeDesignation);
+    }
+
+    function getEmployeeDetails(uint _employeeID) public view returns(uint, string memory, string memory){
+        return(employeeMapping[_employeeID].employeeID, employeeMapping[_employeeID].employeeName, employeeMapping[_employeeID].employeeDesignation);
+    }
+
+}

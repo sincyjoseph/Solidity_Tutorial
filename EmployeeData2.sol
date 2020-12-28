3fda1c0be50a5972ec2d812c09fb33920d2781f6
SINCY JOSEPH <sincyjoseph535@gmail.com>
2020-12-28 13:03:04 +0530

    Update EmployeeData2.sol
    Use of constructor and modifier.

--- EmployeeData2.sol
+++ EmployeeData2.sol
@@ -2,21 +2,12 @@ pragma solidity >= 0.5.0;
 
 contract EmployeeData{
 
-    address owner;
+    address public owner;
     struct EmployeeDB{
         uint employeeID;
         string employeeName;
         string employeeDesignation;
     }
-    
-    function Demo() public{
-        owner = msg.sender;
-    }
-    modifier onlyOwner{
-        require(msg.sender == owner);
-        _;
-    }
-    
     
     event notification(
         uint employeeID,
@@ -24,6 +15,14 @@ contract EmployeeData{
         string employeeDesignation
         );
     mapping(uint => EmployeeDB) public employeeMapping;
+    
+    constructor() public{
+        owner = msg.sender;
+    }
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    }
     
     function setEmployeeDetails(uint _employeeID, string memory _employeeName, string memory _employeeDesignation) public onlyOwner{
         EmployeeDB storage employee = employeeMapping[_employeeID];
